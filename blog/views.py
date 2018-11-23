128b59419f4e224596d5298caf73e07349e3992e
Vinay Singh <singh.vinaypreet@gmail.com>
2018-11-23 13:45:35 -0500

    Till templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+ 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'Vinay',
+        'title': 'Post 1',
+        'content': 'Hello from the other side',
+        'date_posted': 'November 23 2018'
+    },
+    {
+        'author': 'Mr Robot',
+        'title': 'Post 2',
+        'content': 'Hello from the anti side',
+        'date_posted': 'November 23 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', { 'title': 'About'})
