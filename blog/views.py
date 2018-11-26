07f6892e630cac477ddc6e6a2715005302f5c9a0
Vinay Singh <singh.vinaypreet@gmail.com>
2018-11-25 20:53:55 -0500

    User registration form

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
- 
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Vinay',
-        'title': 'Post 1',
-        'content': 'Hello from the other side',
-        'date_posted': 'November 23 2018'
-    },
-    {
-        'author': 'Mr Robot',
-        'title': 'Post 2',
-        'content': 'Hello from the anti side',
-        'date_posted': 'November 23 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
